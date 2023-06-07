2d1adf2af16c036d97334d74f0e5f779e8186d5f
swiftleah <leahrosehenry@gmail.com>
2023-06-07 11:00:04 -0700

    Fixed betty error

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,35 +1,49 @@
 #include "main.h"
 #include <string.h>
 
+/**
+ * is_palindrome - checks if string is palindrome or not
+ * @s: string to be tested
+ *
+ * Return: 1 if palindrome, 0 if not.
+ */
+
 int is_palindrome(char *s)
 {
 	int len = strlen(s);
 
-	if (len == 0 || len == 1)
+	if (len == 0)
 	{
 		return (1);
 	}
 
-	return (palindrome_check(s, 0, len - 1));
+	if (len == 1)
+	{
+		return (1);
+	}
+
+	return (palindrome_checker(s, 0, len - 1));
 }
 
 /**
- * is_palindrome - checks if string is a palindrome or not
+ * palindrome_checker - checks if string is a palindrome or not
  * @s: pointer to characters to test
+ * @start: start of string
+ * @end: end of string
  *
  * Return: 1 if palindrome, 0 if not
  */
 
-int palindrome_check(char *s, int left, int right)
+int palindrome_checker(char *s, int start, int end)
 {
-	if (left >= right)
+	if (start >= end)
 	{
 		return (1);
 	}
 
-	else if (s[left] == s[right])
+	else if (s[start] == s[end])
 	{
-		return (palindrome_check(s, left + 1, right - 1));
+		return (palindrome_checker(s, start + 1, end - 1));
 	}
 
 	else
