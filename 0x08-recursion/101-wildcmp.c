fd2c956b43e434b1dae212cf4929991ecbe97896
swiftleah <leahrosehenry@gmail.com>
2023-06-08 00:48:35 -0700

    Changed back to code with least errors (101wildcmp)

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -27,10 +27,11 @@ int wildcmp(char *s1, char *s2)
 			return (wildcmp(s1, s2 + 1));
 		}
 
-		if (*s1 != '\0' && (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1)))
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
 		{
 			return (1);
 		}
+
 	}
 
 	return (0);
