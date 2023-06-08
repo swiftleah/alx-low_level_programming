4ecb146299c2b29ac8eb3be4848e529f54e89eb1
swiftleah <leahrosehenry@gmail.com>
2023-06-08 00:10:13 -0700

    101-wildcmp file added situation for s1 and s2 identical and s1 != '\0' but s2 = '*'

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -20,5 +20,15 @@ int wildcmp(char *s1, char *s2)
 		return (1);
 	}
 
+	if (*s1 != '\0' && *s2 == '*')
+	{
+		return (wildcmp(s1, s2 + 1));
+	}
+
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+
 	return (0);
 }
