258de71406bbb722ca71b6b84741111252a1e778
swiftleah <leahrosehenry@gmail.com>
2023-06-08 00:42:16 -0700

    101-wildcmp.c file - changed condition of if statement

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -27,7 +27,7 @@ int wildcmp(char *s1, char *s2)
 			return (wildcmp(s1, s2 + 1));
 		}
 
-		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+		if (*s1 != '\0' && (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1)))
 		{
 			return (1);
 		}
