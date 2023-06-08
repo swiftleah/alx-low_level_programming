b6d542ea1b97d58a70cb9d55da35db548afd31c1
swiftleah <leahrosehenry@gmail.com>
2023-06-08 00:05:54 -0700

    Changed 101-wildcmp file to only include for empty s1 and '*' s2

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,36 +1,24 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings
+ * wildcmp - compares two strings to see if they are identical
  * @s1: string 1
  * @s2: string 2
  *
- * Return: 1 if strings are identical, 0 if not
+ * Return: 1 if identical, 0 if not
  */
 
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
+	if (s1[0] == '\0' && s2[0] == '\0')
 	{
 		return (1);
 	}
 
-	if (*s2 == '\0')
+	if (s1[0] == '\0' && *s2 == '*')
 	{
-		return (0);
+		return (1);
 	}
 
-	if (*s2 == '*' || *s1 == *s2)
-	{
-		if (*(s2 + 1) == '*')
-		{
-			return (wildcmp(s1, s2 + 1));
-		}
-
-		if (*s1 != '\0')
-		{
-			return (wildcmp(s1 + 1, s2 + 1));
-		}
-	}
 	return (0);
 }
