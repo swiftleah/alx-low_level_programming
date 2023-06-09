a7982866019427eaf2e769b3e99cfa484e20199f
swiftleah <leahrosehenry@gmail.com>
2023-06-09 05:16:02 -0700

    Got rid of underscore in 3-puts file

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -8,7 +8,7 @@ void _puts(char *str)
  */
 int main(void)
 {
-	_puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
