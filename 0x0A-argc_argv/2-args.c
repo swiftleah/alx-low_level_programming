bb5b067969f48948bdc697611943c6b8c62fd0ec
swiftleah <leahrosehenry@gmail.com>
2023-06-09 00:55:34 -0700

    Fixed betty requirement & improved code for best results

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 
+/**
+ * main - prints all arguments passed to program including program name
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 if successful
+ */
+
 int main(int argc, char *argv[])
 {
 	(void)argc;
 
-	while (*argv)
+	if (argv != NULL)
 	{
-		printf("%s\n", *argv);
-		argv++;
+		while (*argv)
+		{
+			printf("%s\n", *argv);
+			argv++;
+		}
 	}
 	return (0);
 }
