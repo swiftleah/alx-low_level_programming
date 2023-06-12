7678e628b5a28f70de49828965c9f145aafbbe15
swiftleah <leahrosehenry@gmail.com>
2023-06-12 00:56:01 -0700

    Another test , removed an if statement for empty strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,15 +19,10 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		s1 = " ";
+		s1 = "";
 	}
 	if (s2 == NULL)
 	{
-		s2 = " ";
-	}
-	if (s1 == NULL && s2 == NULL)
-	{
-		s1 = "";
 		s2 = "";
 	}
 
