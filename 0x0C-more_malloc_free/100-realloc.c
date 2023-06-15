5517c710cb8b196f0d145274d46f5514c7bead4e
swiftleah <leahrosehenry@gmail.com>
2023-06-15 04:15:06 -0700

    Added if statement

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -20,6 +20,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
 	if (ptr == NULL)
 	{
 		return (malloc(new_size));
