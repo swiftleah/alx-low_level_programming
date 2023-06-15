a02872c3b9f80b67e83cd9581d14b3d68ac5b9e2
swiftleah <leahrosehenry@gmail.com>
2023-06-15 04:06:42 -0700

    Task 4 completed?

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _realloc - reallocates memory using malloc & free
+ * @ptr: ptr to old memory block
+ * @old_size: size in bytes of old memory block
+ * @new_size: size in bytes of new memory block
+ *
+ * Return: ptr to newly allocated memory/NULL upon failure
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new;
+	unsigned int min;
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (ptr == NULL)
+	{
+		return (malloc(new_size));
+	}
+
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+
+	new = malloc(new_size);
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	min = (old_size < new_size) ? old_size : new_size;
+	memcpy(new, ptr, min);
+	free(ptr);
+
+	return (new);
+}
