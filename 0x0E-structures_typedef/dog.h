a5806c05abc3d3773e9265128d9857b269594fe2
swiftleah <leahrosehenry@gmail.com>
2023-06-19 02:12:08 -0700

    Task 0 completed

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct for dog information
+ * @name: name of dog
+ * @age: dog's age in years
+ * @owner: dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
