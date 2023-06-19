6f96538c83edb42d807f8ee5836db777e810044b
swiftleah <leahrosehenry@gmail.com>
2023-06-19 02:14:19 -0700

    Added prototypes & libraries to dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
+/* Libraries: */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Prototypes: */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+
 /**
  * struct dog - struct for dog information
  * @name: name of dog
