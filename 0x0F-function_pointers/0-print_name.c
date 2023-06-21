5f153ebdd5788a9caf1eb4e092b408b116c278b4
swiftleah <leahrosehenry@gmail.com>
2023-06-20 23:45:39 -0700

    Task 0 completed

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints name
+ * @name: name to be printed
+ * @f: pointer to a function
+ *
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+	{
+		f(name);
+	}
+	return;
+}
