605dcf6031672c1ec7901b79ee84649b2bbc56b3
swiftleah <leahrosehenry@gmail.com>
2023-06-21 02:28:32 -0700

    Changed get_op file

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,9 +19,10 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (*(ops[i].op) == *s)
+	while (ops[i].op != NULL)
 	{
-		return (ops[i].f);
+		if (ops[i].op[0] == s[0] && s[1] == '\0')
+			return (ops[i].f);
 		i++;
 	}
 	return (NULL);
