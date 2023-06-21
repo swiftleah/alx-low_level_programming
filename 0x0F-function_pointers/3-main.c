cd5a41324364ce01bdc51770a7b542a14477d48d
swiftleah <leahrosehenry@gmail.com>
2023-06-21 03:43:11 -0700

    Changed argv[2] to num2 in 3-main.c file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && (argv[3] == 0))
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
