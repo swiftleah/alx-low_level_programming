2cc3d5e1824849f545bb778a4613ab23efdfcd8a
swiftleah <leahrosehenry@gmail.com>
2023-06-21 00:47:29 -0700

    Task 2 semi-completed. Not giving correct output.

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,34 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for specific int
+ * @array: array
+ * @size: size of array (number of elements)
+ * @cmp: function pointer that takes int as arg and returns int
+ *
+ * Return: -1 if no element matches or size 0 or less
+ * index of element if element matches
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	if (array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) == 0)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
