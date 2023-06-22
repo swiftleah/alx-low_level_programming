63d54c534fc75d6d6ea301650347975eb7a51c48
swiftleah <leahrosehenry@gmail.com>
2023-06-22 03:04:14 -0700

    Added n <= 0 in both task 1 and 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,7 +17,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
-	if (separator == NULL || n == 0)
+	if (separator == NULL || n <= 0)
 	{
 		va_end(args);
 		return;
