b04e17ccfc3b7c38b481362c161f3117950ff3ca
swiftleah <leahrosehenry@gmail.com>
2023-06-29 03:50:23 -0700

    Fixed betty error in 0-printlist func

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,7 +14,7 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str != NULL)
 		{
-			printf("[%lu] %s\n",strlen(h->str), h->str);
+			printf("[%lu] %s\n", strlen(h->str), h->str);
 		}
 		else
 		{
