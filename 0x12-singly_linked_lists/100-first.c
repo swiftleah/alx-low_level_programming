265206e20a1776556bd78e1cc203492f23a6b7e2
swiftleah <leahrosehenry@gmail.com>
2023-06-29 03:57:45 -0700

    Trying task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print - prints specified message
+ * Return: nothing
+ */
+
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+
+/**
+ * main - prints message
+ * Return: 0
+ */
+
+int main(void);
+{
+	print();
+
+	return (0);
+}
