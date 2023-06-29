c97659a8e2f9d56975255da5146b7ff6bbcab313
swiftleah <leahrosehenry@gmail.com>
2023-06-29 04:06:15 -0700

    Fixed betty error file 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,8 +4,9 @@
  * Return: nothing
  */
 
-void print(void)
+void print_message(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
 }
 
