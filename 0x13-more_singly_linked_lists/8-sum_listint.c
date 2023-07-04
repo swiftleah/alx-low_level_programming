612c6f2f768a772e70a1026dd5c56e2f3b899a04
swiftleah <leahrosehenry@gmail.com>
2023-07-04 00:28:32 -0700

    Fixed betty errors for task 8

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -10,10 +10,10 @@ int sum_listint(listint_t *head)
 	listint_t *current = head;
 	int sum = 0;
 
-        while (current != NULL)
-        {
-                sum += current->n;
-                current = current->next;
-        }
-        return (sum);
-}	
+	while (current != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+	return (sum);
+}
