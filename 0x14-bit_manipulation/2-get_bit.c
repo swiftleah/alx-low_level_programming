c5795367006441fa7b0b6482511a05e9499d6d90
swiftleah <leahrosehenry@gmail.com>
2023-07-06 04:14:26 -0700

    Task 2 completed

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * get_bit - returns value of bit at given index
+ * @n: int
+ * @index: index starting from 0
+ *
+ * Return: value or -1 upon error
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask;
+	unsigned long int bit;
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	mask = 1UL << index;
+	bit = n & mask;
+	return (bit != 0) ? 1 : 0;
+}
