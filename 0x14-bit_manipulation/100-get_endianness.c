950ce3a8999bfdb5986fb130688de7f2a8790465
swiftleah <leahrosehenry@gmail.com>
2023-07-11 01:05:55 -0700

    Added task 100 for 14-bit manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * endianness - checks endianness of system
+ *
+ * Return: int
+ */
+int get_endianness(void)
+{
+	unsigned int num = 1;
+	char *b = (char *)&num;
+
+	return (int)(*b);
+}
