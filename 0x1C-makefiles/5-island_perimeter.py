28f3be81774ba74ce76d2019760e60272f2c99c5
swiftleah <leahrosehenry@gmail.com>
2023-10-25 10:25:18 -0700

    Task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" returns the perimeter of the island described in grid """
+
+def island_perimeter(grid):
+    """ Args: grid
+    Return: perimeter of island in grid
+    """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                perimeter += 4
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
